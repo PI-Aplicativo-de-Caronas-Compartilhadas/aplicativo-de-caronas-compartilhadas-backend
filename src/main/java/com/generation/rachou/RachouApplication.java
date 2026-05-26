c165dc3c137a4865931df3b5c02a4032520b54ee
Roberta Rodrigues <roberta673@gmail.com>
2026-05-26 11:18:38 -0300

    Inicio do Projeto

---
+++ src/main/java/com/generation/rachou/RachouApplication.java
@@ -0,0 +1,13 @@
+package com.generation.rachou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RachouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RachouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/rachou/RachouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.rachou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RachouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
